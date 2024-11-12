de2034a1fbd3a3f5d7bd7a5ae52e4a531c67957d
SandwichPlays <sandwichvtt@gmail.com>
2024-11-12 21:09:53 +0700

    rfgwrwgwrgwrgwre

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
 function navigateTo(url) {
     window.location.href = url;
 }
-
-window.onload = function() {
-    const textElement = document.querySelector('h1');
-    const textContent = textElement.textContent;  // Get the text content
-    textElement.innerHTML = '';  // Clear the current text
-
-    // Loop through each character and wrap it in a <span> tag
-    for (let i = 0; i < textContent.length; i++) {
-        const span = document.createElement('span');
-        span.textContent = textContent[i];  // Set each letter as text content of span
-        textElement.appendChild(span);  // Append the span to the text element
-    }
-};
