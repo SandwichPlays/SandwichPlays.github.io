fa799749f2230a80b45672ce44a2617904e38802
SandwichPlays <sandwichvtt@gmail.com>
2024-11-14 20:25:05 +0700

    Epicness

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function navigateTo(url) {
     window.location.href = url;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const textElements = document.querySelectorAll('.text'); // Get all elements with the class 'wave-text'
+  
+    textElements.forEach((textElement) => {
+      const text = textElement.textContent;
+      textElement.innerHTML = ''; // Clear the existing text
+  
+      text.split('').forEach((char, index) => {
+        const span = document.createElement('span');
+        span.textContent = char;
+        span.classList.add('letter');
+        span.style.animationDelay = `${index * 0.07}s`; // Stagger the animation
+        textElement.appendChild(span);
+      });
+    });
+  });
+  
